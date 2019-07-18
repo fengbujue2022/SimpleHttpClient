afd3a3dd6b0254cb6afe63026ddc7784e38d65ea
frankdeng <frankdeng@openrice.com>
2019-07-18 17:55:27 +0800

    useless commit message( =_= )

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using SimpleHttpClient;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var client = new SimpleHttpClient.HttpClient();
+
+                var request = new HttpRequestMessage(HttpMethod.Get, "http://staging-ap-api.openrice.com.cn");
+
+                var res = await client.SendAsync(request);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
