e2aa28b5020d64e288110d89eee62cfa51543908
Feng-Bu-Jue <978040259@qq.com>
2019-07-29 11:22:30 +0800

    add response parse logic

---
+++ SimpleHttpClient/HttpConnectionResponseContent.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Net.Http
+{
+    internal sealed class HttpConnectionResponseContent : HttpContent
+    {
+        private Stream _stream;
+        private bool _consumedStream; // separate from _stream so that Dispose can drain _stream
+
+        public void SetStream(Stream stream)
+        {
+            _stream = stream;
+        }
+
+        private Stream ConsumeStream()
+        {
+            if (_consumedStream || _stream == null)
+            {
+                throw new InvalidOperationException("stream已经被读取了");
+            }
+            _consumedStream = true;
+
+            return _stream;
+        }
+
+        protected sealed override async Task SerializeToStreamAsync(Stream stream, TransportContext context)
+        {
+            using (Stream contentStream = ConsumeStream())
+            {
+                const int BufferSize = 8192;
+                await contentStream.CopyToAsync(stream, BufferSize).ConfigureAwait(false);
+            }
+        }
+
+        protected sealed override bool TryComputeLength(out long length)
+        {
+            length = 0;
+            return false;
+        }
+
+        protected sealed override Task<Stream> CreateContentReadStreamAsync() =>
+            Task.FromResult<Stream>(ConsumeStream());
+
+
+        protected sealed override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_stream != null)
+                {
+                    _stream.Dispose();
+                    _stream = null;
+                }
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
