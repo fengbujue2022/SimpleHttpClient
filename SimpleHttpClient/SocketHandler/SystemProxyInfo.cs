6ce5ddb926ca938c66e25ad441db353f73e74fac
frankdeng <frankdeng@openrice.com>
2019-12-04 14:46:14 +0800

    feat: support http proxy tunnel

---
+++ SimpleHttpClient/SocketHandler/SystemProxyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SimpleHttpClient
+{
+    public class SystemProxyInfo
+    {
+        public static IWebProxy Proxy => s_proxy.Value;
+
+        private static readonly Lazy<IWebProxy> s_proxy = new Lazy<IWebProxy>(ConstructSystemProxy);
+
+        public static IWebProxy ConstructSystemProxy()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                return HttpEnvironmentProxy.TryCreateWindows(out IWebProxy proxy) ? proxy : new HttpNoProxy();
+            }
+            else if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                if (!HttpEnvironmentProxy.TryCreateWindows(out IWebProxy proxy))
+                {
+                    //HttpWindowsProxy.TryCreate(out proxy);
+                }
+                return proxy ?? new HttpNoProxy();
+            }
+            return new HttpNoProxy();
+        }
+    }
+}
